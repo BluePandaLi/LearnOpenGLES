5e853035e5743f88264e70db891660d0df481d53
BluePandaLi <it20032008@gmail.com>
2017-05-18 19:12:50 +0800

    OpenGL Transform.

---
+++ LearnOpenGLES_5_Transform/LearnOpenGLES_5_Transform/ViewController.mm
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  LearnOpenGLES_5_Transform
+//
+//  Created by BluePandaLi on 18/05/2017.
+//  Copyright © 2017 BluePandaLi. All rights reserved.
+//
+
+#import "ViewController.h"
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans;
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ LearnOpenGLES_5_Transform/LearnOpenGLES_5_Transform/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearnOpenGLES_5_Transform
+//
+//  Created by BluePandaLi on 18/05/2017.
+//  Copyright © 2017 BluePandaLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
